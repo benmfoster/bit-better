6eaa0e06b956f04999bad79e7c39a9986309507f
benjaminjkamp <mail@benjaminjkamp.com>
2019-09-12 01:49:07 -0500

    removes extra files, test code, vex code; removes conflicting css; customizes particle style and background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './App.css';
-import interact from 'interactjs'
 import Particles from 'react-particles-js';
-import Stats from 'stats';
-
-// var particlesJS = require('../public/particles.js-master/particles.js')
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-// particlesJS.load('particles-js', 'assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
-
 
 
 class App extends React.Component {
@@ -143,7 +133,6 @@ newQuestion() {
 displayQuestion() {
   return (
   <div>
-    <Particles />
           <div>
             { this.state.questions[this.state.questionsIndex] }
           </div>
@@ -352,7 +341,7 @@ displayQuestion() {
                 {
                 number:
                   {
-                    value:160,
+                    value:320,
                     density:
                       {
                         enable:true,
@@ -365,7 +354,7 @@ displayQuestion() {
                   },
                 shape:
                   {
-                    "type":"circle",
+                    "type":"star",
                     "stroke":
                       {
                         "width":0,
@@ -393,12 +382,12 @@ displayQuestion() {
                   },
                 size:
                   {
-                    "value":3,
+                    "value":6,
                     "random":true,"anim":
                     {
                       "enable":false,
                       "speed":4,
-                      "size_min":0.3,
+                      "size_min":0.8,
                       "sync":false
                     }
                   },
@@ -491,5 +480,4 @@ displayQuestion() {
 }
 
 export default App;
- // particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#2793c0"},"shape":{"type":"polygon","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":9},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.1763753266952075,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":32.06824121731046,"random":true,"anim":{"enable":false,"speed":211.95215241065108,"size_min":64.9661769841076,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 
